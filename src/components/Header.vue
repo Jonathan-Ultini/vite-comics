067cd51d6d90d32369a50d8c2bf9c7de19527813
Jonathan-Ultini <jonathanultini@gmail.com>
2024-09-16 16:45:03 +0200

    change header css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,9 @@
   <nav class="header">
     <img src="/src/assets/dc-logo.png" alt="DC Logo" class="logo">
     <ul class="menu">
-      <li v-for="item in menuItems" :key="item">{{ item }}</li>
+      <li v-for="item in menuItems" :key="item">
+        <a href="#" class="menu-link">{{ item }}</a>
+      </li>
     </ul>
   </nav>
 </template>
@@ -19,28 +21,34 @@ export default {
 
 <style scoped>
 .header {
+  background-color: white;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 10px 20px;
-  background-color: #fff;
+  border-bottom: 2px solid #ccc;
 }
 
 .logo {
-  width: 50px;
+  width: 100px;
 }
 
 .menu {
   list-style: none;
   display: flex;
-  gap: 20px;
+  gap: 15px;
+  margin: 0;
+  padding: 0;
 }
 
-.menu li {
-  cursor: pointer;
+.menu-link {
+  color: black;
+  text-decoration: none;
+  font-size: 14px;
+  text-transform: uppercase;
 }
 
-.menu li:hover {
-  color: blue;
+.menu-link:hover {
+  border-bottom: 2px solid blue;
 }
 </style>
