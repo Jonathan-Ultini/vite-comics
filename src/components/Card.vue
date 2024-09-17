8a51daacb856ac857f889cad30af2e05c1f34a7c
Jonathan-Ultini <jonathanultini@gmail.com>
2024-09-17 15:38:46 +0200

    install bootsrap, add comic.json, props card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,9 @@
+<!-- components/Card.vue -->
 <template>
   <div class="card">
+    <img :src="thumb" :alt="series" />
     <div class="card-body">
+      <h5 class="card-title">{{ series }}</h5>
     </div>
   </div>
 </template>
@@ -8,9 +11,26 @@
 <script>
 export default {
   props: {
-
+    thumb: String,
+    series: String
   },
 };
 </script>
 
+<style scoped lang="scss">
+.card {
+  width: 200px;
+  margin: 10px;
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+
+  .card-body {
+    background-color: #333;
+    color: white;
+    text-align: center;
+  }
+}
 </style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,25 +1,45 @@
 <template>
-  <main class="main-content">
-    <h2>-- Content goes here --</h2>
+  <main class="main-content container">
+    <h2>Current Series</h2>
+    <div class="row">
+      <Card v-for="(comic, index) in comics" :key="index" :thumb="comic.thumb" :series="comic.series"
+        :price="comic.price" :type="comic.type" />
+    </div>
   </main>
 </template>
 
 <script>
+import Card from './Card.vue';
+import comicsData from '../assets/comics.json';
+
 export default {
   name: 'MainContent',
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      comics: comicsData,
+    };
+  },
 };
 </script>
 
 <style scoped lang="scss">
 .main-content {
-  padding: 20px;
   background-color: #1C1C1C;
-  min-height: 100px;
+  padding: 20px;
 
   h2 {
     color: #fff;
     font-weight: bold;
-    font-size: 30px;
+    margin-bottom: 20px;
+  }
+
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
   }
 }
 </style>
